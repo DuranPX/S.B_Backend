20e404c9a55ef8eca2feda72f16905cf6814d41c
DuranPX <australiaparacall@gmail.com>
2026-05-17 19:48:50 -0500

    Ajustes para conductor

--- ms-business/src/conductor/dto/update-conductor.dto.ts
+++ ms-business/src/conductor/dto/update-conductor.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateConductorDto } from './create-conductor.dto';
+import { IsOptional, IsArray, IsUUID } from 'class-validator';
 
-export class UpdateConductorDto extends PartialType(CreateConductorDto) {}
+export class UpdateConductorDto extends PartialType(CreateConductorDto) {
+  @IsOptional()
+  @IsArray()
+  @IsUUID('4', { each: true })
+  empresasIds?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsUUID('4', { each: true })
+  turnosIds?: string[];
+}
